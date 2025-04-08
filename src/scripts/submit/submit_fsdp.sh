b2bbe68571be25b78427d81d96121760dc0a0d2a
etienneguevel <etienneguev@gmail.com>
2025-04-08 17:01:08 +0200

    feat: put out within log

--- src/scripts/submit/submit_fsdp.sh
+++ src/scripts/submit/submit_fsdp.sh
@@ -6,11 +6,15 @@ source activate /home/guevel/.conda/envs/archehr
 # Define the arguments for the submit.py script
 MODEL_NAME="cross-encoder/nli-deberta-v3-base"
 DATA_PATH="/home/guevel/projects/archehr/data/1.1/dev"
-BATCH_SIZE=128
+BATCH_SIZE=64
 NUM_EPOCHS=300
 LEARNING_RATE=1e-5
-SAVE_NAME="/home/guevel/projects/archehr/logs/fsdp_deberta-v3-base-fc"
+SAVE_NAME="/home/guevel/projects/archehr/logs/fsdp_deberta_fc"
 WORLD_SIZE=4  # Number of GPUs to use
+
+# Make a log file for text outputs
+mkdir -p "$SAVE_NAME"
+LOGFILE="${SAVE_NAME}/output.log"
 
 # Run the submit.py script with the specified arguments
 python /home/guevel/projects/archehr/src/archehr/cross_encode/submit.py \
@@ -20,4 +24,5 @@ python /home/guevel/projects/archehr/src/archehr/cross_encode/submit.py \
     --num_epochs "$NUM_EPOCHS" \
     --learning_rate "$LEARNING_RATE" \
     --save_folder "$SAVE_NAME" \
-    --world_size "$WORLD_SIZE"
+    --world_size "$WORLD_SIZE" \
+    > "$LOGFILE" 2>&1
