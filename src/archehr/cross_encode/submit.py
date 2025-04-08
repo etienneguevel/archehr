cd3513f8d8a7c1fdc3e95e6b4fc7192034fff463
etienneguevel <etienneguev@gmail.com>
2025-04-08 11:27:55 +0200

    fix: change path, fix some variables

--- src/archehr/cross_encode/submit.py
+++ src/archehr/cross_encode/submit.py
@@ -1,5 +1,6 @@
 import os
 
+from archehr import BASE_DIR
 from archehr.cross_encode.train_fsdp import parse_args
 from archehr.utils.cluster import get_idle_gpus
 
@@ -22,13 +23,14 @@ def main():
         raise ValueError("The number of idle GPUs is less than asked.")
 
     # Set CUDA_VISIBLE_DEVICES to the idle GPUs
-    os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, idle_gpus))
-    
+    os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, idle_gpus))    
     print(f"Using GPUs: {os.environ['CUDA_VISIBLE_DEVICES']}")
+
     # Run the training script
-    prompt = " --".join(["{arg} {val}" for arg, val in vars(args).items()])
+    script_path = BASE_DIR / "cross_encode" / "train_fsdp.py"
+    prompt = " ".join([f"--{arg} {val}" for arg, val in vars(args).items()])
     os.system(f"""
-        torchrun --nproc_per_node={args.worls_size} train_fsdp.py{prompt}
+        torchrun --nproc_per_node={args.world_size} {script_path} {prompt}
     """)
 
 
